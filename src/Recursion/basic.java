dd5099245094724926c506f2a5557d348bea739c
MONIKAvv <monikalonewolf35@gmail.com>
2025-05-06 21:52:06 +0530

    recursion

---
+++ src/Recursion/basic.java
@@ -0,0 +1,17 @@
+package Recursion;
+// Recursion love difficulties 😅😅
+
+public class basic {
+//    write a function that prints hello world
+public static void main(String[] args) {
+    message();
+
+}
+static void message(){
+    System.out.println("Hello world!");
+
+}
+}
+//dynamic // heart of dsa,
+// work on recursion
+//Suppose it like----> a function calls another function
