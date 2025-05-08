22c3f19f1457826b0e285f3b76d7a803edc74ec1
MONIKAvv <monikalonewolf35@gmail.com>
2025-05-08 08:18:34 +0530

    binary recursion

--- src/Recursion/BinarySearch.java
+++ src/Recursion/BinarySearch.java
@@ -1,7 +1,29 @@
 package Recursion;
 
-public class BinarySearch {
-    public static void main(String[] args) {
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int [] arr = {1,4,7,3,8,4,0,34,56,2,9};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        int target = 56;
+        System.out.println("The searched Item is : " + (Binary(arr,target,0, arr.length-1)));
+    }
+    static int Binary(int [] arr, int target , int s, int e){
+        int m = s + (e - s ) / 2;
+        if(s > e){
+            return -1;
+        }
+        if(arr[m] == target){
+            return m;
+        }
+
+        if(target > arr[m]){
+            return Binary(arr, target, m+1, e);
+        }
+        return Binary(arr, target, s, m-1);
     }
 }
--- src/Recursion/fabonacciNo.java
+++ src/Recursion/fabonacciNo.java
@@ -18,11 +18,16 @@ public class fabonacciNo {
 //The base condition is registered by answer we already have,
 // in this , we know that f(0) = 0, and f(1) = 1;   this is called base condition
 // Recursion if i can break it down into smaller problems
+
+
 // last recursion function called tailed recursion and if any function call is not last or calling for continue then that is not tailed recursion
+
+
 //How to understand and approach problem?----> Identify if you can break any problem into smaller problems
 //                                         ----> Write the recursion relation if needed
 //                                        ----> Draw the recurive tree
 //About the tree
+
 //see the flow of funcitons, how they are getting in stack
 //Identify and focus on left tree calls and right tree calls
 //Draw the tree and pointer again and again
@@ -42,3 +47,14 @@ public class fabonacciNo {
 // Arguments
 // return type
 // body
+
+
+
+//what are the things that needs to put into the arguments and on body----> The things that are repeative or need in
+// future recursion call and will need to update and will change again and again put that in argument
+// and the things that are not necessary to repeat put that on body
+
+
+
+//Don't overthink just solve the problem and after problem get solved then put your brain into it
+
