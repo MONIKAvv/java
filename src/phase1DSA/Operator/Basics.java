5529330958e1b915359933b2f052d34d36211876
MONIKAvv <monikalonewolf35@gmail.com>
2026-08-07 14:02:02 +0530

    average and multiplication

---
+++ src/phase1DSA/Operator/Basics.java
@@ -0,0 +1,20 @@
+package phase1DSA.Operator;
+
+import java.util.Scanner;
+
+public class Basics {
+    public  static void main(String[] args){
+//        multiplication of two number
+//        take input from user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Three number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+//        int a = 10;
+//        int b = 20;
+        System.out.println("Multiplication of two number is: "+ a*b*c);
+        System.out.println("Average of three number is: "+ ((a+b+c)/3));
+    }
+}
